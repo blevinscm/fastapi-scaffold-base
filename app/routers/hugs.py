726a593e36e88f940b54a5ba0d9661beb908242a
blevinscm <blevinscm@gmail.com>
2020-11-09 17:48:50 -0500

    Saved main and router

--- app/routers/hugs.py
+++ app/routers/hugs.py
@@ -1,29 +1,29 @@
 from fastapi import APIRouter
-from data import models
+from app.data import models 
 
 router = APIRouter()
 
 
 @router.get("/hugs/", tags=["Friendly_Hugs"])
 async def read_hugs():
-    """
+    '''
     Returns all huggers available
-    """
+    '''
     return [{"hugs": "Pooh"}, {"username": "Bear"}]
 
 
 @router.get("/hugs/me", tags=["Friendly_Hugs"])
 async def read_huggee_me():
-    """
+    '''
     Set to return user context. In this case the hugee.
-    """
+    '''
     return {"username": "Huggable Developer"}
 
 
 @router.get("/hugs/{type}", tags=["Friendly_Hugs"])
 async def read_hug_type(hug_type: str):
-    """
+    '''
     Allows user to enter the type of hug they want.
     Replace with any single non-enum type.
-    """
+    '''
     return {"username": hug_type}
