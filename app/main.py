0bb75c5a51090910831229badc729899fdea0dce
blevinscm <blevinscm@gmail.com>
2020-11-11 18:01:23 -0500

    added in html templates

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
-from fastapi import Depends, FastAPI, Header, HTTPException
-
+from fastapi import Request, FastAPI, Header, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse  
 from .routers import hugs
 
-app = FastAPI()
+
+templates = Jinja2Templates(directory="html")
+
+tags_metadata = [
+    {
+        "name": "hugs",
+        "description": "The **hug** is _wonderful_.",
+        "externalDocs": {
+            "description": "Hugs external docs. Use to provide a link.",
+            "url": "https://en.wikipedia.org/wiki/Hug",
+        },
+    },
+  
+]
+app = FastAPI(
+    openapi_tags=tags_metadata,
+    title="The super duper Hugs API",
+    description="This is an example for the base project Generator",
+    version="0.0.0.0.0.0.1",
+
+)
 
 
 async def get_token_header(x_token: str = Header(...)):
@@ -11,3 +32,7 @@ async def get_token_header(x_token: str = Header(...)):
 
 
 app.include_router(hugs.router)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
